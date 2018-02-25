66dc76479465c0df13f40b91e0ff5a966d6cd802
lukaszfranczak <lukaszfranczak@gmail.com>
2018-02-25 15:16:30 +0100

    Render SkiResort list

--- src/components/SkiResorts.jsx
+++ src/components/SkiResorts.jsx
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 import './SkiResorts.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      skiResorts: [],
+    }
+  }
+
+  renderSkiResort(resort) {
+    return null
+  }
+
   render() {
     return (
       <div className="SkiResortsWrapper">
+        <ul className="SkiResortsList">
+          {this.state.skiResorts.map(resort => this.renderSkiResort(resort))}
+        </ul>
       </div>
     );
   }
